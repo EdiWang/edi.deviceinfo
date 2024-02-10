9946e2575bd203ee9c5f671e78446ba43c278e1a
Edi Wang <Edi.Wang@outlook.com>
2024-02-10 10:29:43 +0800

    Edi.DeviceInfo lib

---
+++ DotNet/Edi.DeviceInfo.Tests/WebDeviceTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Edi.DeviceInfo.Tests;
+
+[TestClass]
+public class WebDeviceTests
+{
+    [TestMethod]
+    public void GetDeviceHash_IgnoreScreenRotationAndPlatformVersion_ReturnsExpectedHash()
+    {
+        // Arrange
+        var webDevice = new WebDevice
+        {
+            CanvasFingerprint = "abc123",
+            ScreenResolution = new ScreenResolution(1920, 1080),
+            ScreenTotalPixels = 2073600,
+            TimeZone = "UTC",
+            UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            Platform = "Windows",
+            PlatformVersion = "10.0",
+            Architecture = "x64"
+        };
+
+        var expectedHash = "iqhIHfv3LEm4gc4mNQ/sDvG7f9yBT/SDzcPKBgSSO2I=";
+
+        // Act
+        var actualHash = webDevice.GetDeviceHash();
+
+        // Assert
+        Assert.AreEqual(expectedHash, actualHash);
+    }
+
+    [TestMethod]
+    public void GetDeviceHash_WithScreenRotation_ExpectedHash()
+    {
+        // Arrange
+        var webDevice = new WebDevice
+        {
+            CanvasFingerprint = "abc123",
+            ScreenResolution = new ScreenResolution(1920, 1080),
+            ScreenTotalPixels = 2073600,
+            TimeZone = "UTC",
+            UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            Platform = "Windows",
+            PlatformVersion = "10.0",
+            Architecture = "x64"
+        };
+
+        var expectedHash = "z8AY7gny9Z4o605s13PsGXwkR6lP+KWAzbkMcFrie4g=";
+
+        // Act
+        var actualHash = webDevice.GetDeviceHash(ignoreScreenRotation: false);
+
+        // Assert
+        Assert.AreEqual(expectedHash, actualHash);
+    }
+
+    [TestMethod]
+    public void GetDeviceHash_WithPlatformVersion_ReturnsExpectedHash()
+    {
+        // Arrange
+        var webDevice = new WebDevice
+        {
+            CanvasFingerprint = "abc123",
+            ScreenResolution = new ScreenResolution(1920, 1080),
+            ScreenTotalPixels = 2073600,
+            TimeZone = "UTC",
+            UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            Platform = "Windows",
+            PlatformVersion = "10.0",
+            Architecture = "x64"
+        };
+
+        var expectedHash = "5USsFCIMPfAC9WF4Paa9ZVeZSs7dUuInoXPM79ke4xY=";
+
+        // Act
+        var actualHash = webDevice.GetDeviceHash(ignorePlatformVersion: false);
+
+        // Assert
+        Assert.AreEqual(expectedHash, actualHash);
+    }
+}
---
+++ DotNet/Edi.DeviceInfo/WebDevice.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Edi.DeviceInfo;
+
+public class WebDevice
+{
+    public string CanvasFingerprint { get; set; }
+    public ScreenResolution ScreenResolution { get; set; }
+    public long ScreenTotalPixels { get; set; }
+    public string TimeZone { get; set; }
+    public string UserAgent { get; set; }
+    public string Platform { get; set; }
+    public string PlatformVersion { get; set; }
+    public string Architecture { get; set; }
+
+    public string GetDeviceHash(bool ignoreScreenRotation = true, bool ignorePlatformVersion = true)
+    {
+        var screenInfo = ignoreScreenRotation ? $"{ScreenTotalPixels}" : $"{ScreenResolution.W}x{ScreenResolution.H}";
+        var platformInfo = ignorePlatformVersion ? $"{Platform}" : $"{Platform}{PlatformVersion}";
+
+        SHA256 sha256 = SHA256.Create();
+        byte[] inputBytes = Encoding.ASCII.GetBytes($"{CanvasFingerprint}{screenInfo}{TimeZone}{platformInfo}");
+        byte[] hash = sha256.ComputeHash(inputBytes);
+
+        return Convert.ToBase64String(hash);
+    }
+}
+
+public record ScreenResolution(int W, int H);
